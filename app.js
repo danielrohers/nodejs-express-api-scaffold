dcd25d984ee033e989b81097f5ff533510381aa6
Daniel Röhers Moura <rohersmoura@gmail.com>
2016-10-03 14:11:22 -0300

    added helmet

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
